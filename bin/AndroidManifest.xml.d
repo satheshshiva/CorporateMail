2854c98209f06e5cdb90f75ea45e470ecd57229c
Sathesh <satheshshiva@gmail.com>
2014-08-24 18:32:00 -0700

    added back bin folder

---
+++ bin/AndroidManifest.xml.d
@@ -0,0 +1,2 @@
+/Users/sathesh/MyContent/workspaces/workspace_android/WiproMailFree/bin/AndroidManifest.xml : \
+/Users/sathesh/MyContent/workspaces/workspace_android/WiproMailFree/AndroidManifest.xml \
