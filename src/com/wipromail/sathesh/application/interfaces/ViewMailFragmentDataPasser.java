7e1165a712f4bcfa0328fdedacea80d45b364174
Sathesh <satheshshiva@gmail.com>
2014-09-07 01:35:31 -0700

    ViewMail. OncLickListener moved to a different package

---
+++ src/com/wipromail/sathesh/application/interfaces/ViewMailFragmentDataPasser.java
@@ -0,0 +1,42 @@
+/**
+ * 
+ */
+package com.wipromail.sathesh.application.interfaces;
+
+import com.wipromail.sathesh.service.data.EmailMessage;
+
+/**
+ * @author sathesh
+ *
+ */
+public interface ViewMailFragmentDataPasser {
+
+	/**
+	 * 
+	 */
+	public void showAlertdialogPermanentDelete();
+
+	/**
+	 * @throws Exception 
+	 * 
+	 */
+	public void forwardMail() throws Exception;
+
+	/**
+	 * @param b
+	 * @throws Exception 
+	 */
+	public void replyMail(boolean b) throws Exception;
+
+	/**
+	 * @return
+	 */
+	public EmailMessage getMessage();
+
+	/**
+	 * @return
+	 */
+	public int getMailType();
+
+
+}
