8ad1ea3f688caba69f607355a602073dee731ae9
satheshshiva <satheshshiva@gmail.com>
2014-09-10 23:01:05 -0700

    Initial Check on migration to Android Studio - Gradle

--- src/com/wipromail/sathesh/asynctask/interfaces/GenericAsyncTask.java
+++
@@ -1,8 +0,0 @@
-package com.wipromail.sathesh.asynctask.interfaces;
-
-public interface GenericAsyncTask {
-public void activity_OnPreExecute();
-public void activity_onProgressUpdate(String... progress);
-public void activity_OnPostExecute();
-
-}
--- src/com/wipromail/sathesh/service/data/WindowsLiveCredentials.java
+++
@@ -1,5 +0,0 @@
-package com.wipromail.sathesh.service.data;
-
-public class WindowsLiveCredentials extends WSSecurityBasedCredentials{
-
-}
