237ebfebf97b21bac262a6e4b10fa5f40c2e6270
Sathesh <satheshshiva@gmail.com>
2014-11-03 01:17:23 -0800

    REFERENCE: Reverted RecyclerView to ListView. Reference can be made from this Commit  for the MailListViewFragment if using the Recycler View in future.

--- app/src/main/java/com/wipromail/sathesh/ui/listeners/MailListViewListener.java
+++ app/src/main/java/com/wipromail/sathesh/ui/listeners/MailListViewListener.java
@@ -45,7 +45,7 @@ public class MailListViewListener implements  OnScrollListener, OnItemClickListe
 		try {
 			//Determine whether its our listview listener
 			switch(lw.getId()) {
-			case R.id.recyclerView:
+			case R.id.listView:
 				//Log.d(TAG, "First Visible: " + firstVisibleItem + ". Visible Count: " + visibleItemCount+ ". Total Items:" + totalItemCount);
 
 				//SWIPE REFRESH
