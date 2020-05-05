a13295778088b660ed411f7ea74643c2daccf5f7
Sathesh Sivashanmugam <sathesh.sivashanmugam@aexp.com>
2020-05-04 22:52:26 -0700

    replaced shared preference commit() with apply()

--- app/src/main/java/com/sathesh/corporatemail/activity/MyPreferencesActivity.java
+++ app/src/main/java/com/sathesh/corporatemail/activity/MyPreferencesActivity.java
@@ -31,7 +31,6 @@ import java.util.Calendar;
  */
 public class MyPreferencesActivity extends PreferenceActivity implements Constants ,OnSharedPreferenceChangeListener{
 
-    public static int OFFICE365_URL_POSITION = 0;
     public static int CUSTOM_URL_POSITION = 3;
 
     public final static String KEY_WEBMAIL_SERVER="webmail_server";
