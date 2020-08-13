d3fd238a6119024413b9b4d7e9a3ac2c1b35dcc0
Sathesh Sivashanmugam <sathesh.sivashanmugam@aexp.com>
2020-08-13 11:02:17 -0700

    removed fitSystemWindows and applied paddingTop

--- app/src/main/java/com/sathesh/corporatemail/activity/AddRecipientActivity.java
+++ app/src/main/java/com/sathesh/corporatemail/activity/AddRecipientActivity.java
@@ -56,7 +56,7 @@ public class AddRecipientActivity extends MyActivity implements Constants,IResol
 	private final ArrayList<ContactSerializable> selectedList = new ArrayList<ContactSerializable>();
 
 	@Override
-	protected void onCreate(Bundle savedInstanceState) {
+	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 
 		setContentView(R.layout.activity_add_recipient);
